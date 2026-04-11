d2972d3b86738126327b16c15cc27e8495482bc4
Jose Oquendo <jmoquendocarloni@gmail.com>
2026-04-11 13:11:02 -0300

    homepage working

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>¡Mi proyecto está vivo! 🚀</h1>
+        <p>La arquitectura y DB están listas. Ahora agrego el frontend.</p>
+      </div>
+    );
+  }
